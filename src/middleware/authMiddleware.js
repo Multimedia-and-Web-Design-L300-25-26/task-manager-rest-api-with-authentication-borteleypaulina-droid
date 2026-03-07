d976fb11c2beb3d0c7452dcd87751c79ed563f77
borteleypaulina-droid <255503586+borteleypaulina-droid@users.noreply.github.com>
2026-03-07 10:08:20 -1200

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,16 +1,12 @@
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
+const app = express();
 
-// 1. Extract token from Authorization header
-// 2. Verify token
-// 3. Find user
-// 4. Attach user to req.user
-// 5. Call next()
-// 6. If invalid → return 401
+app.use(express.json());
 
-const authMiddleware = async (req, res, next) => {
-  //  implement here
-};
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
-export default authMiddleware;
+export default app;
